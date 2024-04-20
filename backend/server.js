6200b9aab2301f70833a66960d2b106f5d5f71d9
Zarah <devvick230385@gmail.com>
2024-04-20 20:53:33 +0100

    UPDATE: initialized and constructed backend service

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { sequelize } = require("./models");
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(morgan("common"));
+const server = app;
+const PORT = 5000 || process.env.PORT
+server.listen(5000, async () => {
+    sequelize.authenticate();
+    console.log("server running on port ", PORT);
+
+});
