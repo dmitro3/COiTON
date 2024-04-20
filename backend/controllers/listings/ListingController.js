55de444b4e285a08805cce3ff997215a0752d0de
Zarah <devvick230385@gmail.com>
2024-04-21 00:35:43 +0100

    UPDATE: added listing approval workflow

--- backend/controllers/listings/ListingController.js
+++ backend/controllers/listings/ListingController.js
@@ -1,4 +1,5 @@
 const { ResponseMessage } = require("../../helpers/Response");
+const { SendListingTransaction } = require("../../helpers/SendListingTransaction");
 const { Listing } = require("../../models");
 
 exports.createListing = async (req, res) => {
@@ -6,6 +7,29 @@ exports.createListing = async (req, res) => {
         const data = req.body;
         const newListing = await Listing.create({ details: data });
         return ResponseMessage(res, true, 200, "Listing created successfully", newListing);
+    } catch (error) {
+        console.log(error.message)
+        ResponseMessage(res, false, 500, error.message ?? "Internal server error", {});
+    }
+}
+
+
+exports.approveListing = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const listing = await Listing.findByPk(id)
+        if (listing) {
+            const transaction = await SendListingTransaction(id, listing.details);
+            if (transaction.success) {
+                return ResponseMessage(res, true, 200, "Listing approved successfully", tx);
+
+            } else {
+                return ResponseMessage(res, false, 400, transaction.message, transaction.tx);
+
+            }
+        } else {
+            return ResponseMessage(res, false, 400, "Listing not found", {});
+        }
     } catch (error) {
         console.log(error.message)
         ResponseMessage(res, false, 500, error.message ?? "Internal server error", {});
--- backend/routes/listings/ListingRoutes.js
+++ backend/routes/listings/ListingRoutes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const { ListingController } = require("../../controllers")
+const { ListingController } = require("../../controllers");
+const VerifyToken = require("../../helpers/VerifyToken");
 
 router.get("/", ListingController.getListings);
 router.post("/", ListingController.createListing);
+router.post("/:id", VerifyToken, ListingController.approveListing);
 router.delete("/:id", ListingController.deleteListing);
 
 
