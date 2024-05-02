b5ddebef10650bb4fd792c83fe4800b9bb9a420e
Abdullahi Salihu <abdullahisalihuinusa@gmail.com>
2024-05-02 20:48:02 +0100

    deployed to sepolia

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,23 +1,25 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-require('@nomiclabs/hardhat-ethers')
-// const dotenv = require("dotenv")
-// dotenv.config();
-// const RPC_URL = process.env.RPC_URL;
-// const PRIVATE_KEY = process.env.PRIVATE_KEY;
+require("@nomiclabs/hardhat-ethers");
+const dotenv = require("dotenv");
+dotenv.config();
+const RPC_URL = process.env.RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 // const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 module.exports = {
-  solidity: '0.8.24',
+  solidity: "0.8.24",
 
   networks: {
-    // sepolia: {
-    //   url: RPC_URL,
-    //   accounts: [PRIVATE_KEY]
-    // },
+    sepolia: {
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
     anvil: {
       url: "http://127.0.0.1:8545",
-      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
-    }
+      accounts: [
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+      ],
+    },
   },
-}
+};
--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -1,5 +1,5 @@
-const { getSelectors, FacetCutAction } = require('./libraries/diamond.js')
-const { ethers } = require('hardhat')
+const { getSelectors, FacetCutAction } = require("./libraries/diamond.js");
+const { ethers } = require("hardhat");
 const fs = require("fs");
 
 async function deployDiamond() {
@@ -7,101 +7,96 @@ async function deployDiamond() {
   // const contractOwner = accounts[0]
 
   // deploy DiamondCutFacet
-  const DiamondCutFacet = await ethers.getContractFactory('DiamondCutFacet')
-  const diamondCutFacet = await DiamondCutFacet.deploy()
-  await diamondCutFacet.deployed()
-  console.log('DiamondCutFacet deployed:', diamondCutFacet.address)
-  const ACCOUNT = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  const DiamondCutFacet = await ethers.getContractFactory("DiamondCutFacet");
+  const diamondCutFacet = await DiamondCutFacet.deploy();
+  await diamondCutFacet.deployed();
+  console.log("DiamondCutFacet deployed:", diamondCutFacet.address);
+  const ACCOUNT = "0xC0E11e7674B3267175569e1c42b85bB5554aFEB4";
   // deploy Diamond
-  const Diamond = await ethers.getContractFactory('Diamond')
+  const Diamond = await ethers.getContractFactory("Diamond");
   const diamond = await Diamond.deploy(
     ACCOUNT,
     diamondCutFacet.address,
     ACCOUNT
-  )
-  await diamond.deployed()
-
+  );
+  await diamond.deployed();
 
   // deploy DiamondInit
   // DiamondInit provides a function that is called when the diamond is upgraded to initialize state variables
   // Read about how the diamondCut function works here: https://eips.ethereum.org/EIPS/eip-2535#addingreplacingremoving-functions
-  const DiamondInit = await ethers.getContractFactory('DiamondInit')
-  const diamondInit = await DiamondInit.deploy()
-  await diamondInit.deployed()
-  console.log('DiamondInit deployed:', diamondInit.address)
+  const DiamondInit = await ethers.getContractFactory("DiamondInit");
+  const diamondInit = await DiamondInit.deploy();
+  await diamondInit.deployed();
+  console.log("DiamondInit deployed:", diamondInit.address);
 
+  const ERC20Token = await ethers.getContractFactory("CoitonERC20");
+  const erc20Token = await ERC20Token.deploy(ACCOUNT);
 
-
-  const ERC20Token = await ethers.getContractFactory("CoitonERC20")
-  const erc20Token = await ERC20Token.deploy(ACCOUNT)
-
-
-
-
-  const ERC721Token = await ethers.getContractFactory("CoitonNFT")
-  const erc721Token = await ERC721Token.deploy()
+  const ERC721Token = await ethers.getContractFactory("CoitonNFT");
+  const erc721Token = await ERC721Token.deploy();
 
   await erc20Token.deployed();
-  console.log("Deployed erc20 token at", erc20Token.address)
+  console.log("Deployed erc20 token at", erc20Token.address);
   await erc721Token.deployed();
-  console.log("Deployed erc721 token at", erc721Token.address)
-
-
-
+  console.log("Deployed erc721 token at", erc721Token.address);
 
   await diamond.setToken(erc20Token.address, erc721Token.address);
 
   // deploy facets
-  console.log('')
-  console.log('Deploying facets')
-  const FacetNames = ['DiamondLoupeFacet', 'OwnershipFacet', 'RealEstate', 'Trade']
+  console.log("");
+  console.log("Deploying facets");
+  const FacetNames = [
+    "DiamondLoupeFacet",
+    "OwnershipFacet",
+    "RealEstate",
+    "Trade",
+  ];
 
-  const cut = []
+  const cut = [];
   for (const FacetName of FacetNames) {
-    const Facet = await ethers.getContractFactory(FacetName)
-    const facet = await Facet.deploy()
-    await facet.deployed()
-    console.log(`${FacetName} deployed: ${facet.address}`)
+    const Facet = await ethers.getContractFactory(FacetName);
+    const facet = await Facet.deploy();
+    await facet.deployed();
+    console.log(`${FacetName} deployed: ${facet.address}`);
     cut.push({
       facetAddress: facet.address,
       action: FacetCutAction.Add,
       functionSelectors: getSelectors(facet),
-    })
+    });
   }
 
-  const Dao = await ethers.getContractFactory("Dao")
+  const Dao = await ethers.getContractFactory("Dao");
   const dao = await Dao.deploy(diamond.address);
   await dao.deployed();
 
   console.log("Deployed Dao to ", dao.address);
 
   // upgrade diamond with facets
-  console.log('')
-  console.log('Diamond Cut:', cut)
-  const diamondCut = await ethers.getContractAt('IDiamondCut', diamond.address)
-  let tx
-  let receipt
+  console.log("");
+  console.log("Diamond Cut:", cut);
+  const diamondCut = await ethers.getContractAt("IDiamondCut", diamond.address);
+  let tx;
+  let receipt;
   // call to init function
-  let functionCall = diamondInit.interface.encodeFunctionData('init')
-  tx = await diamondCut.diamondCut(cut, diamondInit.address, functionCall)
-  console.log('Diamond cut tx: ', tx.hash)
-  receipt = await tx.wait()
+  let functionCall = diamondInit.interface.encodeFunctionData("init");
+  tx = await diamondCut.diamondCut(cut, diamondInit.address, functionCall);
+  console.log("Diamond cut tx: ", tx.hash);
+  receipt = await tx.wait();
   if (!receipt.status) {
-    throw Error(`Diamond upgrade failed: ${tx.hash}`)
+    throw Error(`Diamond upgrade failed: ${tx.hash}`);
   }
-  console.log('Completed diamond cut')
-  console.log("\n-------------------------------------\n")
-  console.log('Diamond deployed at:', diamond.address)
-
+  console.log("Completed diamond cut");
+  console.log("\n-------------------------------------\n");
+  console.log("Diamond deployed at:", diamond.address);
 
   const daoData = {
     contractAddress: dao.address,
     admin: ACCOUNT,
     agent: ACCOUNT,
-  }
+  };
   fs.writeFileSync(`scripts/interractions/data.json`, JSON.stringify(daoData));
 
-  return diamond.address
+  return diamond.address;
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -110,9 +105,9 @@ if (require.main === module) {
   deployDiamond()
     .then(() => process.exit(0))
     .catch((error) => {
-      console.error(error)
-      process.exit(1)
-    })
+      console.error(error);
+      process.exit(1);
+    });
 }
 
-exports.deployDiamond = deployDiamond
+exports.deployDiamond = deployDiamond;
