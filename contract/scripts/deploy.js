639488012326d2e48b8771ce7c2befbff5531686
zarah <devvick230385@gmail.com>
2024-05-08 13:05:35 +0100

    UPDATE: switched to anvil

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -11,7 +11,7 @@ async function deployDiamond() {
   const diamondCutFacet = await DiamondCutFacet.deploy();
   await diamondCutFacet.deployed();
   console.log("DiamondCutFacet deployed:", diamondCutFacet.address);
-  const ACCOUNT = "0xC0E11e7674B3267175569e1c42b85bB5554aFEB4";
+  const ACCOUNT = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
   // deploy Diamond
   const Diamond = await ethers.getContractFactory("Diamond");
   const diamond = await Diamond.deploy(
