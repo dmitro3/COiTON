aef17f11c2c48c325a5cfa95b9a635be3686bead
zarah <devvick230385@gmail.com>
2024-05-29 12:53:26 +0100

    FIX: result return type (uint)

--- backend/helpers/Response.js
+++ backend/helpers/Response.js
@@ -1,3 +1,3 @@
 exports.ResponseMessage = (res, success, statusCode, message, data) => {
-    return res.status(statusCode).send({ success, message, data:{...data,id:5} });
+    return res.status(statusCode).send({ success, message, data:{id:5,...data} });
 }
