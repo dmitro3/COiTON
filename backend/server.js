1d1b8712ef635c372549fa8441d7949d473f52d4
zarah <devvick230385@gmail.com>
2024-06-01 00:29:34 +0100

    UPDATE: implemented image generator

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,33 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(morgan("common"));
+const axios = require("axios");
 
 app.get("/", (req, res) => {
   res.status(200).send("server running successfully");
+});
+
+app.get("/yo", async (req, res) => {
+  const apiKey = process.env.API_KEY; // Your API key, securely managed
+  const data = {
+    model: "dall-e-2",
+    prompt: "a girl",
+    n: 1,
+    size: "1024x1024",
+  };
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apiKey}`,
+  };
+  const response = await axios.post(
+    "https://api.openai.com/v1/images/generations",
+    data,
+    {
+      headers,
+    }
+  );
+
+  res.status(200).send({ data: response.data.data[0] });
 });
 
 app.use("/api/v1", AppRoutes);
